975c667dc997c051f812c3ace9308991c22105b2
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-09-23 20:16:32 -0300

    Update Conexao.php

--- exercicios/04/dao/connect/Conexao.php
+++ exercicios/04/dao/connect/Conexao.php
@@ -3,7 +3,7 @@
         public static function getConexao(){
             // $conexao = new PDO("TIPO_BANCO:host=ERVIDOR;dbname=NOME_BANCO", "USUARIO", "SENHA"); 
             
-            $conexao = new PDO("mysql:host=sql110.epizy.com;dbname=epiz_30865194_bdEtecCidadeTiradentes;charset=utf8", "epiz_30865194", "RfvxflNcb7GnJa5");
+            $conexao = new PDO("mysql:host=localhost;dbname=bd_site", "root", "");
             $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
             return $conexao;
